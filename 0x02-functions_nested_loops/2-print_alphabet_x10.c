cf70cf611cf3d163be69c31740f02011621510da
Menor25 <ajirimenor@gmail.com>
2021-08-31 13:43:44 -1200

    Betty error corrected

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code for the alphabet
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  * Return: Always 0.
